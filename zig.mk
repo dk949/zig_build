ab83d532c59cd79bf48b9e553e8867c34db148e1
dk949 <56653556+dk949@users.noreply.github.com>
2022-10-17 13:42:46 +0100

    added build mode

--- zig.mk
+++ zig.mk
@@ -1,3 +1,5 @@
+ZIG_BUILD_MODE := Debug
+
 ZIG_PREFIX := $(PWD)/zig
 LLVM_PREFIX := $(PWD)/llvm
 ZSTD_PREFIX := $(PWD)/zstd
@@ -13,24 +15,24 @@ progress/zig-patch: progress/zig-clone
 	touch $@
 
 progress/zig-cmake-config: progress/zig-patch
-	cmake -B zig-source/build-release -S zig-source \
+	cmake -B zig-source/build-$(ZIG_BUILD_MODE) -S zig-source \
 	  -DCMAKE_INSTALL_PREFIX=$(ZIG_PREFIX) \
 	  -DCMAKE_PREFIX_PATH="$(LLVM_PREFIX)" \
 	  -DZIG_USE_LLVM_CONFIG=On \
 	  -DZIG_FIX_SYSTEM_LIBS=On \
 	  -DZIG_USE_CCACHE=On \
-	  -DCMAKE_BUILD_TYPE=Release
+	  -DCMAKE_BUILD_TYPE=$(ZIG_BUILD_MODE)
 	touch $@
 
 progress/zig-build: progress/zig-cmake-config
-	cmake --build zig-source/build-release
+	cmake --build zig-source/build-$(ZIG_BUILD_MODE)
 	touch $@
 
 progress/zig: progress/zig-build
 	@echo "installing zig into $(ZIG_PREFIX)"
 	@sleep 5
 	mkdir -p $(ZIG_PREFIX)
-	cmake --install zig-source/build-release
+	cmake --install zig-source/build-$(ZIG_BUILD_MODE)
 	touch $@
 
 
@@ -38,11 +40,11 @@ clean:
 	rm -f progress/zig progress/zig-build
 
 clean-comp: clean
-	cmake --build zig-source/build-release --target clean
+	cmake --build zig-source/build-$(ZIG_BUILD_MODE) --target clean
 
 clean-cmake: clean
 	rm -f progress/zig-cmake-config
-	rm -rf zig-source/build-release
+	rm -rf zig-source/build-$(ZIG_BUILD_MODE)
 
 clean-deep:
 	rm -f progress/zig*
